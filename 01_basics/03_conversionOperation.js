9ae994d2ed7aa8bcfb384fc5d3d43d83c6c2bde6
yayush059 <108602300+yayush059@users.noreply.github.com>
2024-08-01 16:32:07 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,82 @@
+let score = 30              //number
+console.log(typeof score)   //number
+console.log(typeof(score))  //number
+
+console.log()
+
+let age = "30"              //string type
+console.log(age)
+console.log(typeof age)     //string
+let valueInteger = Number(age)
+console.log(valueInteger)
+console.log(typeof(valueInteger))    // converted to number
+
+console.log()
+
+let score_card = "30abc"              //string type
+console.log(score_card)
+console.log(typeof score_card)        //string
+let scoreInt = Number(score_card)     //number
+console.log(scoreInt)                   //nan
+console.log(typeof scoreInt)
+
+console.log()
+
+let num = null
+console.log(num)
+console.log(typeof num)
+let numInt = Number(num)
+console.log(numInt)
+console.log(typeof numInt)
+
+console.log()
+
+let num1 = undefined
+console.log(num1)
+console.log(typeof num1)
+let num1Int = Number(num1)
+console.log(num1Int)
+console.log(typeof num1Int)
+
+console.log()
+
+let num2 = true
+console.log(num2)
+console.log(typeof num2)
+let num2Int = Number(num2)
+console.log(num2Int)
+console.log(typeof num2Int)
+
+// "33" (string)         -> 33 (number)
+// "33abc" (string)      -> NaN (number)
+// true (boolean)        -> 1 (number)
+// undefined (undefined) -> NaN (number)   
+// null (object)         -> 0 (number)  
+
+console.log()
+
+let isloggedIn = "3"
+console.log(isloggedIn)
+console.log(typeof isloggedIn)
+let booleanLoggedIn = Boolean(isloggedIn)
+console.log(booleanLoggedIn)
+console.log(typeof booleanLoggedIn)
+
+console.log()
+
+// 0 (number)      -> false (boolean)
+// !=0 (number)    -> true (boolean)
+// "" (string)     -> false (boolean)
+// "djfh" (string) -> true (boolean)
+
+
+let somenum = 3
+console.log(somenum)
+console.log(typeof somenum)
+let Stringnum = String(somenum)
+console.log(Stringnum)
+console.log(typeof Stringnum)
+
+console.log()
+
+// 3 (number) -> 3 (string)
