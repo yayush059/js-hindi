aa30491a6a9355fd49b12d4dab5f59f1e528b41a
yayush059 <108602300+yayush059@users.noreply.github.com>
2024-08-03 16:04:32 +0000

    datatypes summary

---
+++ 01_basics/05_dataTypes_summary.js
@@ -0,0 +1,49 @@
+//    PRIMITIVE DATA types
+//    7 types : String, Number, Boolean, null, undefined, Symbol, BIGInt
+
+// js is dynamic langauge coz there is no need define the type of the varibles
+
+
+// Reference (Non primitive)
+// Array, obejtcs, functions
+const score = 100
+const scoreValue = 100.3
+
+const loggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123')                   //typeof -> Symbol
+const userId = Symbol('123')               //typeof -> Symbol
+// console.log(id==userId)
+// console.log(id===userId)
+
+
+const BIGnum = 54123654125896569855n
+//console.log(typeof BIGnum)    //bigint
+
+const heros = ["shaktiman", "naagraaj", "doga"]
+let myObj ={
+    name: "Ayush",
+    age: 22,
+}
+
+console.log(heros)
+console.log(myObj)
+
+const myFun = function(){
+    console.log("Hello World")         //typeof for function will give function
+}
+
+/****************    SUMMARY   ******************/
+/*  typeof
+    null       -> object
+    undefined  -> undefined
+    boolean    -> boolean
+    number     -> number
+    string     -> string
+    function   -> function
+    symbol     -> symbol
+    biginteger -> bigint
+
+*/
