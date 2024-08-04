1567a8e9cc116e9b3001230efa1097e9a600023e
yayush059 <108602300+yayush059@users.noreply.github.com>
2024-08-04 16:33:19 +0000

    arrays basics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,44 @@
+const myArr = [1,2,3,4,'ayush',true]
+const myArr1 = ["ayush", "suyash", "rajat", "piyush"]
+const myArr2 = new Array(0,1,2,3,4,5)
+
+//console.log(myArr)
+
+//console.log(`length of arry is ${myArr.length}`)
+//console.log(myArr.flat(2))
+
+//array methods
+
+// myArr2.push(6)
+// myArr2.push(7)
+// myArr2.pop()
+
+
+// myArr2.unshift(-1)      //to add first
+// myArr2.shift()          //to delete first
+// console.log(myArr2)
+
+//QUESTIONS
+// console.log(myArr2.includes(2))
+// console.log(myArr2.includes(9))
+// console.log(myArr[0])
+// console.log(myArr.indexOf(4))
+
+// const newArr = myArr2.join()
+// console.log(myArr2)
+// console.log(newArr)
+// console.log(typeof newArr)
+
+//slice , splice
+
+console.log("myArr2", myArr2)
+console.log(myArr2.slice(1,3))
+console.log("myArr2", myArr2)
+console.log()
+
+console.log("myArr2", myArr2)
+console.log(myArr2.splice(1,3))
+console.log("myArr2", myArr2)
+console.log()
+
+
