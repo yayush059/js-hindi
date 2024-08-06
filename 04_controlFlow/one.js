a7da996bb6b8e82e4c975bb3056fc6c770014025
yayush059 <108602300+yayush059@users.noreply.github.com>
2024-08-06 12:00:46 +0000

    control flow

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,55 @@
+// const userLoggedid = true
+
+// if(3!=2){
+//     //this will be executed
+//     console.log("Ayush")
+// }
+
+// if(2=="2"){
+//     //this will be executed
+//     console.log("Yadav")
+// }
+
+// const balance =1000
+
+// if(balance>500) console.log("Yes");
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedinfromemail = true;
+
+if(userLoggedIn && debitCard){
+    console.log("allow for shopping")
+}
+
+// switch (key) {
+//     case value:
+//         break;
+
+//     case value:
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("JAN")
+        break;
+    case 2:
+        console.log("FEB")
+        break;
+    case 3:
+        console.log("MAR")
+        break;
+    case 4:
+        console.log("APR")
+        break;
+    default:
+        console.log("DEFAULT CASE MATCH")
+        break;
+}
