637f07a593925afc86a2e63c0c04ab70edcc158b
yayush059 <108602300+yayush059@users.noreply.github.com>
2024-08-07 07:30:51 +0000

    switch function

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,19 @@
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("JAN")
+        break;
+    case 2:
+        console.log("FEB")
+        break;
+    case 3:
+        console.log("MAR")
+        break;
+    case 4:
+        console.log("APR")
+        break;
+    default:
+        console.log("DEFAULT CASE MATCH")
+        break;
+}
