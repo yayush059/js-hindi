94052fd65ca739050319358ce893c8b31f89e9f1
yayush059 <108602300+yayush059@users.noreply.github.com>
2024-08-07 07:31:44 +0000

    else if

--- 04_controlFlow/one.js
+++ 04_controlFlow/one.js
@@ -33,23 +33,3 @@ if(userLoggedIn && debitCard){
 //     default:
 //         break;
 // }
-
-const month = 3
-
-switch (month) {
-    case 1:
-        console.log("JAN")
-        break;
-    case 2:
-        console.log("FEB")
-        break;
-    case 3:
-        console.log("MAR")
-        break;
-    case 4:
-        console.log("APR")
-        break;
-    default:
-        console.log("DEFAULT CASE MATCH")
-        break;
-}
