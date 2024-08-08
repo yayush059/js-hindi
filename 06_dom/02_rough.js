05aec9b1840b4ed934f65b44161270ec5623c288
yayush059 <108602300+yayush059@users.noreply.github.com>
2024-08-08 17:14:52 +0000

    rough

--- 06_dom/02_rough.js
+++ 06_dom/02_rough.js
@@ -2,3 +2,12 @@
 // document.getElementById('firstHeading')
 // document.getElementById('firstHeading').innerHTML="<h1>chai aur code</h1>"
 // to change the elemnt in the web page
+
+// changing color of h2 in any page
+
+// document.querySelectorAll('.mw-heading')
+// const allmwheading = document.querySelectorAll('.mw-heading')
+// allmwheading.forEach(function(h){
+//     h.style.color="red"
+// )
+
